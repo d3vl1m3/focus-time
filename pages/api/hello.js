a9b6a9b169f5df34974f39c6f664e4dadef7e033
D3VL1M3 <dev_lime@protonmail.com>
2021-11-25 22:35:09 +0000

    fix: eslint still playing up

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,1 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-const handler = (req, res) => res.status(200).json({ name: 'John Doe' });
-export default handler;
+export const handler = (req, res) => res.status(200).json({ name: 'John Doe' });
